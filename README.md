eb1bd5e8e5447c8e1ed05f7b684c308dd6ff94af
DanilaZaytsev <lfyz20@mail.ru>
2024-04-26 15:50:39 +0300

    Добавил снятие rtsp потока

---
+++ README.md
@@ -0,0 +1,5 @@
+  активировать venv
+  Поставить pip install -r requirements.txt
+  
+##TODO
+Доделать API
---
+++ rtsp_capt.py
@@ -0,0 +1,47 @@
+import cv2
+import os
+from datetime import datetime
+import schedule
+import time
+
+def capture_rtsp_stream(rtsp_url, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    cap = cv2.VideoCapture(rtsp_url)
+
+    if not cap.isOpened():
+        print("Ошибка: Не удалось установить соединение с RTSP потоком")
+        return
+
+    ret, frame = cap.read()
+
+    if ret:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        filename = os.path.join(output_dir, f"snapshot_{timestamp}.jpg")
+
+        cv2.imwrite(filename, frame)
+        print(f"Скриншот сохранен как {filename}")
+    else:
+        print("Ошибка: Не удалось получить кадр из RTSP потока")
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def capture_screenshot_job(rtsp_url, output_dir):
+    capture_rtsp_stream(rtsp_url, output_dir)
+
+def setup_schedule(rtsp_url, output_dir, schedule_times):
+    for time_str in schedule_times:
+        schedule.every().day.at(time_str).do(capture_screenshot_job, rtsp_url, output_dir)
+
+# Параметры
+rtsp_url = "ваш_rtsp_url"
+output_dir = "путь_к_вашей_директории"
+schedule_times = ["10:00", "12:00", "14:00", "16:00", "18:00", "20:00"] #время пар
+
+setup_schedule(rtsp_url, output_dir, schedule_times)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
