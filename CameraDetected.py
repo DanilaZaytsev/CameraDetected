e651dc716e90285c3f6974806b9939b31832cb79
DanilaZaytsev <lfyz20@mail.ru>
2024-04-28 01:45:35 +0300

    TG-2 Прикрутил API

--- CameraDetected.py
+++ CameraDetected.py
@@ -3,25 +3,16 @@ import os
 from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 from imageai.Detection import VideoObjectDetection
+import asyncio
 
 app = Flask(__name__)
 
-def is_allowed_filename(filename):
-    # Возвращает True, если имя файла допустимо
-    return "." in filename
-
-
-unique_persons = set()
-
-
-def count_unique_persons(frame_number, output_array, output_count, returned_frame):
-    global unique_persons
+async def count_unique_persons(frame_number, output_array, output_count, returned_frame, unique_persons):
     if "person" in output_count:
         unique_persons.update(["person_" + str(i) for i in range(output_count["person"])])
         # Используем множество для хранения уникальных идентификаторов
 
-def detect_objects_from_video(video_file_path):
-    # Функция для обнаружения объектов в видеофайле
+async def detect_objects_from_video(video_file_path, unique_persons):
     execution_path = os.getcwd()
     detector = VideoObjectDetection()
     detector.setModelTypeAsYOLOv3()
@@ -30,8 +21,9 @@ def detect_objects_from_video(video_file_path):
 
     video_path = detector.detectObjectsFromVideo(
         input_file_path=video_file_path,
-        output_file_path=os.path.join(execution_path, "pidors_detected"),
-        frames_per_second=10, log_progress=True, per_frame_function=count_unique_persons, return_detected_frame=True
+        output_file_path=os.path.join(execution_path, "debug"),
+        frames_per_second=10, log_progress=True, per_frame_function=count_unique_persons, return_detected_frame=True,
+        unique_persons=unique_persons
     )
 
     unique_persons_list = list(unique_persons)
@@ -43,21 +35,33 @@ def detect_objects_from_video(video_file_path):
 
     return unique_persons_count, video_path
 
+async def process_video(file, unique_persons):
+    filename = secure_filename(file.filename)
+    video_file_path = os.path.join("uploads", filename)
+    file.save(video_file_path)
+    unique_persons_count, video_path = await detect_objects_from_video(video_file_path, unique_persons)
+    return {"unique_persons_count": unique_persons_count, "video_path": video_path}
+
+
+def is_allowed_filename(filename):
+    ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route("/detect_objects", methods=["POST"])
-def detect_objects():
+async def detect_objects():
     if 'file' not in request.files:
         return jsonify({"error": "No file part"})
     file = request.files['file']
     if file.filename == '':
         return jsonify({"error": "No selected file"})
     if file and is_allowed_filename(file.filename):
-        filename = secure_filename(file.filename)
-        video_file_path = os.path.join("uploads", filename)
-        file.save(video_file_path)
-        unique_persons_count, video_path = detect_objects_from_video(video_file_path)
-        return jsonify({"unique_persons_count": unique_persons_count, "video_path": video_path})
+        unique_persons = set()
+        task = asyncio.create_task(process_video(file, unique_persons))
+        result = await task
+        return jsonify(result)
     else:
         return jsonify({"error": "Invalid file name"})
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True, port=8080)
--- async_rtsp_capt.py
+++ async_rtsp_capt.py
@@ -2,10 +2,10 @@ import cv2
 import os
 import asyncio
 import time
+import requests
 from datetime import datetime
-import aiortc
 
-async def capture_rtsp_stream(rtsp_url, output_dir, identifier):
+async def capture_rtsp_stream(rtsp_url, output_dir, identifier, api_url):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
@@ -23,30 +23,39 @@ async def capture_rtsp_stream(rtsp_url, output_dir, identifier):
 
         cv2.imwrite(filename, frame)
         print(f"Скриншот от RTSP потока {rtsp_url} сохранен как {filename}")
+
+        # Отправка файла на сервер Flask
+        files = {'file': open(filename, 'rb')}
+        response = requests.post(api_url, files=files)
+
+        if response.status_code == 200:
+            print("Файл успешно отправлен на сервер Flask")
+        else:
+            print("Произошла ошибка при отправке файла на сервер Flask")
+
     else:
         print(f"Ошибка: Не удалось получить кадр из RTSP потока {rtsp_url}")
 
     cap.release()
 
 # Функция для создания скриншотов по расписанию
-async def capture_screenshots(rtsp_streams, output_dir):
-    tasks = [capture_rtsp_stream(rtsp_url, output_dir, identifier) for identifier, rtsp_url in rtsp_streams.items()]
+async def capture_screenshots(rtsp_streams, output_dir, api_url):
+    tasks = [capture_rtsp_stream(rtsp_url, output_dir, identifier, api_url) for identifier, rtsp_url in rtsp_streams.items()]
     await asyncio.gather(*tasks)
 
-async def start_screenshot_scheduler(rtsp_streams, output_dir, schedule_times):
+async def start_screenshot_scheduler(rtsp_streams, output_dir, schedule_times, api_url):
     while True:
-
         now = datetime.now().strftime("%H:%M")
         if now in schedule_times:
-            await capture_screenshots(rtsp_streams, output_dir)
+            await capture_screenshots(rtsp_streams, output_dir, api_url)
         await asyncio.sleep(30)
 
 rtsp_streams = {
-    "alias1": "ваш_rtsp_url1",
-    "alias22": "ваш_rtsp_url2"
-}  # словарь с идентификаторами и RTSP потоками
-output_dir = "путь_к_вашей_директории"
+    "alias1": "rtsp_url1",
+    "alias22": "rtsp_url2"
+}  # список rtsp потоков с камер  и их алиасы(к прим.номера аудиторий)
+output_dir = "куда складывать"
 schedule_times = {"10:00", "12:00", "14:00", "16:00", "18:00", "20:00"}
+api_url = "http://localhost:порт/detect_objects"
 
-
-asyncio.run(start_screenshot_scheduler(rtsp_streams, output_dir, schedule_times))
+asyncio.run(start_screenshot_scheduler(rtsp_streams, output_dir, schedule_times, api_url))
